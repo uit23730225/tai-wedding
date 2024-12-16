1d79fe3f4ad9c54bc34d6d09455940927ab9ffe4
uit23730225 <23730225@ms.uit.edu.vn>
2024-12-16 22:42:41 +0700

    new wedding

--- src/screens/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FrontPageInvitation } from './FrontPageInvitation/FrontPageInvitation'
-export { default as WeddingInvitation } from './WeddingInvitation/WeddingInvitation'
-export { default as MainScreen } from './MainScreen/MainScreen'
